55df828c44d33f5d77d135b0c009c84aecbbe0b0
ghaza <ghaza@gmail.com>
2023-03-05 07:51:01 +0700

    add GUI Program

---
+++ TPMOD3_1302210093/TPMOD3_1302210093/Form1.cs
@@ -0,0 +1,31 @@
+namespace TPMOD3_1302210093
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            label1.Text = "Helo " + text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
